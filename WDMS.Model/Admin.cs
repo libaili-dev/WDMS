48155297d67d703a8856b5e56db4156cadc32290
Linfi.Li <liboli.1988@hotmail.com>
2016-04-04 11:59:57 +0800

    add models

---
+++ WDMS.Model/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDMS.Model
+{
+    public class Admin
+    {
+        public int AdminId { get; set; }
+
+        public int AdminRoleId { get; set; }
+
+        public string AdminName { get; set; }
+
+        public string Password { get; set; }
+
+        public string AdminRoleAuthority { get; set; }
+    }
+}
---
+++ WDMS.Model/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDMS.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public int CustomerType { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public int Gender { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public DateTime FittingDate { get; set; }
+
+        public DateTime FinalFittingDate { get; set; }
+
+        public DateTime WeddingDate { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public int Operator { get; set; }
+
+    }
+
+
+    public class CustomerType
+    {
+        public int CustomerTypeId { get; set; }
+
+        public string CustomerTypeName { get; set; }
+
+        public float Discount { get; set; }
+
+
+    }
+
+}
