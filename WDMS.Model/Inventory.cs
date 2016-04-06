d8af979ce3cd9f1e240c879ea47d18d1d77c08af
Linfi.Li <liboli.1988@hotmail.com>
2016-04-06 13:12:19 +0800

    add models

---
+++ WDMS.Model/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDMS.Model
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+
+        public int StyleId { get; set; }
+
+        public string Size { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public int RentingCount { get; set; }
+
+        public int WashingCount { get; set; }
+
+        public string Status { get; set; }
+
+        public decimal RentPrice { get; set; }
+
+        public decimal SellPrice { get; set; }
+
+        public string Remark { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+
+        public int Operator { get; set; }
+
+    }
+}
