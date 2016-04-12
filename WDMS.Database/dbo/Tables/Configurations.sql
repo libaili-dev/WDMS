442dcc07f2dea7330c0e2865b1d3faf9cb181f6a
Linfi.Li <liboli.1988@hotmail.com>
2016-04-13 00:45:42 +0800

    add database table Configurations

---
+++ WDMS.Database/dbo/Tables/Configurations.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Configurations] (
+    [ConfigId]    INT           IDENTITY (1, 1) NOT NULL,
+    [ConfigKey]   NVARCHAR (50) NOT NULL,
+    [ConfigValue] NVARCHAR (50) NULL,
+    [ConfigGroup] NVARCHAR (50) NOT NULL,
+    [Description] NVARCHAR (50) NULL,
+    CONSTRAINT [PK_Configurations] PRIMARY KEY CLUSTERED ([ConfigId] ASC)
+);
+
