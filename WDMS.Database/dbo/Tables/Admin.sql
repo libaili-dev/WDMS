789b6961141b2b0ef2e14cc9c32f96cdf056d143
Linfi.Li <liboli.1988@hotmail.com>
2016-04-23 21:36:56 +0800

    Add PK realtionship

--- WDMS.Database/dbo/Tables/Admin.sql
+++ WDMS.Database/dbo/Tables/Admin.sql
@@ -2,7 +2,10 @@
     [AdminId]     INT           IDENTITY (1, 1) NOT NULL,
     [AdminRoldId] INT           NOT NULL,
     [AdminName]   NVARCHAR (50) NOT NULL,
-    [Password]    NVARCHAR (50) NULL, 
-    CONSTRAINT [PK_Admin] PRIMARY KEY ([AdminId])
+    [Password]    NVARCHAR (50) NULL,
+    CONSTRAINT [PK_Admin] PRIMARY KEY CLUSTERED ([AdminId] ASC),
+    CONSTRAINT [FK_Admin_AdminRole] FOREIGN KEY ([AdminRoldId]) REFERENCES [dbo].[AdminRole] ([RoleId])
 );
+
+
 
--- WDMS.Database/dbo/Tables/OrderDetails.sql
+++ WDMS.Database/dbo/Tables/OrderDetails.sql
@@ -1,16 +1,20 @@
 ﻿CREATE TABLE [dbo].[OrderDetails] (
-    [OrderDatailId]           INT             IDENTITY (1, 1) NOT NULL,
-    [OrderBatchId] INT             NOT NULL,
-    [OrderType]    NVARCHAR (50)   NULL,
-    [InventoryId]  INT             NOT NULL,
-    [DeliveryWay]  NVARCHAR (10)   NULL,
-    [ExpressNo]    NVARCHAR (50)   NULL,
-    [Price]        DECIMAL (15, 2) NULL,
-    [Status]       NVARCHAR (50)   NULL,
-    [Operator]     INT             NOT NULL,
-	[CreateTime]	DATETIME CONSTRAINT [DF_OrderDetails_CreateTime] DEFAULT (getdate()) NOT NULL,
-	[UpdateTime]	DATETIME null,
-    [Remark]       NVARCHAR (200)  NULL, 
-    CONSTRAINT [PK_OrderDetails] PRIMARY KEY ([OrderDatailId])
+    [OrderDatailId] INT             IDENTITY (1, 1) NOT NULL,
+    [OrderBatchId]  INT             NOT NULL,
+    [OrderType]     NVARCHAR (50)   NULL,
+    [InventoryId]   INT             NOT NULL,
+    [DeliveryWay]   NVARCHAR (10)   NULL,
+    [ExpressNo]     NVARCHAR (50)   NULL,
+    [Price]         DECIMAL (15, 2) NULL,
+    [Status]        NVARCHAR (50)   NULL,
+    [Operator]      INT             NOT NULL,
+    [CreateTime]    DATETIME        CONSTRAINT [DF_OrderDetails_CreateTime] DEFAULT (getdate()) NOT NULL,
+    [UpdateTime]    DATETIME        NULL,
+    [Remark]        NVARCHAR (200)  NULL,
+    CONSTRAINT [PK_OrderDetails] PRIMARY KEY CLUSTERED ([OrderDatailId] ASC),
+    CONSTRAINT [FK_OrderDetails_Inventory] FOREIGN KEY ([InventoryId]) REFERENCES [dbo].[Inventory] ([InventoryId]),
+    CONSTRAINT [FK_OrderDetails_Orders] FOREIGN KEY ([OrderBatchId]) REFERENCES [dbo].[Orders] ([OrderBatchId])
 );
+
+
 
