b1ebe026d366c35499240a7fefcb1878fed19969
Linfi.Li <liboli.1988@hotmail.com>
2016-04-29 09:47:53 +0800

    add missing form files

---
+++ WDMS.WinForm/FormQueryCustomer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WDMS.WinForm
+{
+    public partial class FormQueryCustomer : Form
+    {
+        public FormQueryCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            string keyword = this.txtKeyword.Text.Trim();
+            if (string.IsNullOrEmpty(keyword))
+            {
+                MessageBox.Show("关键词不能为空！");
+            }
+            else
+            {
+                //
+            }
+        }
+
+        private void txtKeyword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnQuery_Click(sender, e);
+            }
+        }
+    }
+}
---
+++ WDMS.WinForm/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WDMS.WinForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newCustomerSearchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormNewCustomer fmNewCustomer = new FormNewCustomer();
+            fmNewCustomer.ShowDialog();
+        }
+
+        private void queryCustomerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormQueryCustomer frm = new FormQueryCustomer();
+            frm.ShowDialog();
+        }
+    }
+}
