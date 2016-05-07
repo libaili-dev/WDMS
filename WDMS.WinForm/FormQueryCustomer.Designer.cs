06b5e2fa500ac4ce9e3aef2770c3c3a755cecb9c
Linfi.Li <liboli.1988@hotmail.com>
2016-05-07 15:35:50 +0800

    tmp checkin

--- WDMS.WinForm/FormQueryCustomer.Designer.cs
+++ WDMS.WinForm/FormQueryCustomer.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WDMS.WinForm
 {
-    partial class FormQueryCustomer
+    partial class FormQueryAdmin
     {
         /// <summary>
         /// Required designer variable.
@@ -31,11 +31,11 @@
             this.lblInputCustomerInfo = new System.Windows.Forms.Label();
             this.txtKeyword = new System.Windows.Forms.TextBox();
             this.btnQuery = new System.Windows.Forms.Button();
-            this.gridCustomers = new System.Windows.Forms.DataGridView();
+            this.gridAdmin = new System.Windows.Forms.DataGridView();
             this.panelMessage = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblMessage = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.gridCustomers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAdmin)).BeginInit();
             this.panelMessage.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -43,41 +43,41 @@
             // lblInputCustomerInfo
             // 
             this.lblInputCustomerInfo.AutoSize = true;
-            this.lblInputCustomerInfo.Location = new System.Drawing.Point(26, 13);
+            this.lblInputCustomerInfo.Location = new System.Drawing.Point(26, 14);
             this.lblInputCustomerInfo.Name = "lblInputCustomerInfo";
-            this.lblInputCustomerInfo.Size = new System.Drawing.Size(125, 12);
+            this.lblInputCustomerInfo.Size = new System.Drawing.Size(43, 13);
             this.lblInputCustomerInfo.TabIndex = 0;
-            this.lblInputCustomerInfo.Text = "客户姓名或联系方式：";
+            this.lblInputCustomerInfo.Text = "姓名：";
             // 
             // txtKeyword
             // 
-            this.txtKeyword.Location = new System.Drawing.Point(157, 10);
+            this.txtKeyword.Location = new System.Drawing.Point(157, 11);
             this.txtKeyword.Name = "txtKeyword";
-            this.txtKeyword.Size = new System.Drawing.Size(354, 21);
+            this.txtKeyword.Size = new System.Drawing.Size(354, 20);
             this.txtKeyword.TabIndex = 1;
             this.txtKeyword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtKeyword_KeyPress);
             // 
             // btnQuery
             // 
-            this.btnQuery.Location = new System.Drawing.Point(548, 8);
+            this.btnQuery.Location = new System.Drawing.Point(548, 9);
             this.btnQuery.Name = "btnQuery";
-            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.Size = new System.Drawing.Size(75, 25);
             this.btnQuery.TabIndex = 2;
             this.btnQuery.Text = "查找(&S)";
             this.btnQuery.UseVisualStyleBackColor = true;
             this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
             // 
-            // gridCustomers
+            // gridAdmin
             // 
-            this.gridCustomers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.gridAdmin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.gridCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridCustomers.Location = new System.Drawing.Point(12, 54);
-            this.gridCustomers.Name = "gridCustomers";
-            this.gridCustomers.RowTemplate.Height = 23;
-            this.gridCustomers.Size = new System.Drawing.Size(731, 437);
-            this.gridCustomers.TabIndex = 3;
+            this.gridAdmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAdmin.Location = new System.Drawing.Point(12, 59);
+            this.gridAdmin.Name = "gridAdmin";
+            this.gridAdmin.RowTemplate.Height = 23;
+            this.gridAdmin.Size = new System.Drawing.Size(731, 473);
+            this.gridAdmin.TabIndex = 3;
             // 
             // panelMessage
             // 
@@ -85,9 +85,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelMessage.Controls.Add(this.panel1);
-            this.panelMessage.Location = new System.Drawing.Point(12, 493);
+            this.panelMessage.Location = new System.Drawing.Point(12, 534);
             this.panelMessage.Name = "panelMessage";
-            this.panelMessage.Size = new System.Drawing.Size(731, 44);
+            this.panelMessage.Size = new System.Drawing.Size(731, 48);
             this.panelMessage.TabIndex = 4;
             // 
             // panel1
@@ -96,31 +96,31 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(410, 44);
+            this.panel1.Size = new System.Drawing.Size(410, 48);
             this.panel1.TabIndex = 0;
             // 
             // lblMessage
             // 
             this.lblMessage.AutoSize = true;
             this.lblMessage.ForeColor = System.Drawing.Color.Fuchsia;
-            this.lblMessage.Location = new System.Drawing.Point(4, 20);
+            this.lblMessage.Location = new System.Drawing.Point(4, 22);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(0, 12);
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
             this.lblMessage.TabIndex = 0;
             // 
-            // FormQueryCustomer
+            // FormQueryAdmin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(755, 537);
+            this.ClientSize = new System.Drawing.Size(755, 582);
             this.Controls.Add(this.panelMessage);
-            this.Controls.Add(this.gridCustomers);
+            this.Controls.Add(this.gridAdmin);
             this.Controls.Add(this.btnQuery);
             this.Controls.Add(this.txtKeyword);
             this.Controls.Add(this.lblInputCustomerInfo);
-            this.Name = "FormQueryCustomer";
-            this.Text = "客户信息查询";
-            ((System.ComponentModel.ISupportInitialize)(this.gridCustomers)).EndInit();
+            this.Name = "FormQueryAdmin";
+            this.Text = "管理员信息查询";
+            ((System.ComponentModel.ISupportInitialize)(this.gridAdmin)).EndInit();
             this.panelMessage.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -134,7 +134,7 @@
         private System.Windows.Forms.Label lblInputCustomerInfo;
         private System.Windows.Forms.TextBox txtKeyword;
         private System.Windows.Forms.Button btnQuery;
-        private System.Windows.Forms.DataGridView gridCustomers;
+        private System.Windows.Forms.DataGridView gridAdmin;
         private System.Windows.Forms.Panel panelMessage;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblMessage;
